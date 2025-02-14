c13323ebf6d1ce9759a57595d000b69e7da848d6
Sudhir Singh <142374717+sudhir25464@users.noreply.github.com>
2025-02-14 17:04:35 +0530

    code added

--- src/component/TopSearchBar.jsx
+++ src/component/TopSearchBar.jsx
@@ -3,14 +3,14 @@ import "../CSS/header.css";
 import "../CSS/filterMenuContainer.css";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import "../CSS/pop.css"
+import "../CSS/pop.css";
 
 const ShowPopComponent = ({ imageUrl }) => {
   return (
     <div className="show-container">
-    <div className="popup">
-      <img src={imageUrl} alt="Popup" className="popup-image" />
-    </div>
+      <div className="popup">
+        <img src={imageUrl} alt="Popup" className="popup-image" />
+      </div>
     </div>
   );
 };
@@ -18,72 +18,54 @@ const ShowPopComponent = ({ imageUrl }) => {
 const LoadingComponent = () => {
   return (
     <div className="loading-container">
-    <div className="loading-popup">
-
-      {/* <img src={imageUrl} alt="Popup" className="popup-image" /> */}
-    </div>
+      <div className="loading-popup">
+        {/* <img src={imageUrl} alt="Popup" className="popup-image" /> */}
+      </div>
     </div>
   );
 };
 
 function TopSearchBar() {
+  const [imagestore, setimageStore] = useState([""]);
 
-
-  const [imagestore, setimageStore] = useState([""])
-
-  const [imageUrl, setImageUrl] = useState(null)
-
-  
+  const [imageUrl, setImageUrl] = useState(null);
 
   const [LoadingState, setloadingState] = useState(false);
-
 
   const [showPopup, setShowPopup] = useState(false);
 
   const [userinput, setUserInput] = useState({
     prompt: "",
     magic_prompt: true,
-
   });
 
   const HandleSubmit = async (e) => {
-    
     e.preventDefault();
+
     console.log("User input:", userinput);
-    setloadingState(true)
-    
 
     try {
-  
-      const response = await axios.post("http://127.0.0.1:5000/gen-post", userinput);
+      setloadingState(true);
+      const response = await axios.post(
+        "http://127.0.0.1:5000/gen-post",
+        userinput
+      );
       // setUserInput(response);
       setImageUrl(response.data.image_url);
-     
-      // setimageStore(response.data);
-
-      // const rsp = await response
-
-
 
       console.log("Data sent successfully:", response.data);
 
-
-      
       // alert("Form submitted successfully!");
       setloadingState(false);
       setShowPopup(true);
-      
+
       setUserInput({ prompt: "" });
-   
     } catch (error) {
       console.log("error is:", error);
     }
   };
 
-
-
   const [image, seTimage] = useState([]);
-
 
   const getAllimage = async () => {
     try {
@@ -100,20 +82,13 @@ function TopSearchBar() {
     // getAllimage();
   }, []);
 
-
-
-  const loading= ()=>{
-    return(
+  const loading = () => {
+    return (
       <div class="spinner-border text-primary" role="status">
         <span class="sr-only">Loading...</span>
       </div>
-    )
-  }
-
-
- 
-  
-
+    );
+  };
 
   return (
     <>
@@ -126,70 +101,46 @@ function TopSearchBar() {
           <div className="search-inner-container">
             <div className="search-input">
               <input
-
                 type="text"
                 value={userinput.userRequest}
                 onChange={(e) =>
                   setUserInput({ ...userinput, prompt: e.target.value })
                 }
                 placeholder="Describe what you want to see. Your prompt cannot be empty."
-                required/>
+                required
+              />
               <div>
-
-
-
-                    <button
-              type="submit"
-              onClick={HandleSubmit}
-              className={`generate-btn ${loading ? "loading" : ""}`}
-              disabled={loading} // Disable button when loading
-            >
-              {loading ? "Generating..." : "Generate"}
-            </button>
-                {/* <button
+                <button
                   type="submit"
                   onClick={HandleSubmit}
                   className="generate-btn"
                 >
-                     Generate
-                </button> */}
+                  Generate
+                </button>
               </div>
-
 
               {/* new component */}
 
-
-              {
-                LoadingState ? (
-                    <div>
-
-                    {/* <p>loading..</p> */}
-                       <LoadingComponent/>
-                    </div>
-                ) :(
-                    <div>
-
-                       <ShowPopComponent  imageUrl={imageUrl}/>
-                    </div>
-                )
-                
-              
-
-                
-
-              }
+              {LoadingState ? (
+                <div>
+                  <p>loading..</p>
+                  {/* <LoadingComponent/> */}
+                </div>
+              ) : (
+                <div>
+                  <ShowPopComponent imageUrl={imageUrl} />
+                </div>
+              )}
             </div>
           </div>
         </div>
       </div>
-      
+
       {/* {
         imageUrl?
         <img src={imageUrl} />
         :""
       } */}
-      
-      
 
       {/* bottom-section    filter-menu*/}
 
@@ -212,17 +163,11 @@ function TopSearchBar() {
         {/* imagelist -container */}
 
         <div className="image-container">
-        
-       {
-
-          imagestore.map((image, index) => (
+          {imagestore.map((image, index) => (
             <div key={index}>
               <img src={image.image_url} alt="image" />
             </div>
-          ))
-
-         }
-      
+          ))}
         </div>
       </div>
     </>
